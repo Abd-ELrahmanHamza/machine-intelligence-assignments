fc40e7cc3656440ce927575a091a10e8de483afd
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-11-06 00:41:40 +0200

    [check point] flod fill heuristic

--- Problem Set 1/sokoban_heuristic.py
+++ Problem Set 1/sokoban_heuristic.py
@@ -1,20 +1,48 @@
-from sokoban import SokobanProblem, SokobanState
+from collections import deque
+from typing import List
+
+from sokoban import SokobanProblem, SokobanState, SokobanLayout
 from mathutils import Direction, Point, manhattan_distance
 from helpers.utils import NotImplemented
+
 
 # This heuristic returns the distance between the player and the nearest crate as an estimate for the path cost
 # While it is consistent, it does a bad job at estimating the actual cost thus the search will explore a lot of nodes before finding a goal
 def weak_heuristic(problem: SokobanProblem, state: SokobanState):
     return min(manhattan_distance(state.player, crate) for crate in state.crates) - 1
 
-#TODO: Import any modules and write any functions you want to use
+
+# TODO: Import any modules and write any functions you want to use
+
+def flod_fill(layout: SokobanLayout) -> List[List[int]]:
+    area = layout.width * layout.height
+    graph = [[area for i in range(layout.width)] for j in range(layout.height)]
+    def bfs_flod_fill():
+        queue = deque()
+        for goal in layout.goals:
+            queue.append(goal)
+            graph[goal.y][goal.x] = 0
+        while queue:
+            point = queue.popleft()
+            for direction in Direction:
+                new_point = point + direction.to_vector()
+                if new_point in layout.walkable and graph[new_point.y][new_point.x] == area:
+                    graph[new_point.y][new_point.x] = graph[point.y][point.x] + 1
+                    queue.append(new_point)
+
+    bfs_flod_fill()
+    return graph
 
 
 def strong_heuristic(problem: SokobanProblem, state: SokobanState) -> float:
-    #TODO: ADD YOUR CODE HERE
-    #IMPORTANT: DO NOT USE "problem.get_actions" HERE.
+    # TODO: ADD YOUR CODE HERE
+    # IMPORTANT: DO NOT USE "problem.get_actions" HERE.
     # Calling it here will mess up the tracking of the expanded nodes count
     # which is the number of get_actions calls during the search
-    #NOTE: you can use problem.cache() to get a dictionary in which you can store information that will persist between calls of this function
+    # NOTE: you can use problem.cache() to get a dictionary in which you can store information that will persist between calls of this function
     # This could be useful if you want to store the results heavy computations that can be cached and used across multiple calls of this function
-    NotImplemented()
+    cache = problem.cache()
+    if 'graph' not in cache:
+        cache['graph'] = flod_fill(problem.layout)
+    graph = cache['graph']
+    return max(graph[crate.y][crate.x] for crate in state.crates)
