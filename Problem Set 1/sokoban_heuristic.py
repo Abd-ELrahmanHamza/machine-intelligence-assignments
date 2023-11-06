5bdf02142e08d38b254083b2a5894e5b7d69988b
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-11-06 23:33:10 +0200

    refactor code

--- Problem Set 1/sokoban_heuristic.py
+++ Problem Set 1/sokoban_heuristic.py
@@ -15,7 +15,7 @@ def weak_heuristic(problem: SokobanProblem, state: SokobanState):
 # TODO: Import any modules and write any functions you want to use
 
 def flod_fill(layout: SokobanLayout) -> List[List[int]]:
-    area = layout.width * layout.height
+    area = 100000
     graph = [[area for i in range(layout.width)] for j in range(layout.height)]
 
     def bfs_flod_fill():
@@ -28,7 +28,7 @@ def flod_fill(layout: SokobanLayout) -> List[List[int]]:
             for direction in Direction:
                 new_point = point + direction.to_vector()
                 if new_point in layout.walkable and graph[new_point.y][new_point.x] == area:
-                    graph[new_point.y][new_point.x] = graph[point.y][point.x] + 1
+                    graph[new_point.y][new_point.x] = (graph[point.y][point.x] + 1)
                     queue.append(new_point)
 
     bfs_flod_fill()
@@ -60,11 +60,16 @@ def strong_heuristic(problem: SokobanProblem, state: SokobanState) -> float:
     # which is the number of get_actions calls during the search
     # NOTE: you can use problem.cache() to get a dictionary in which you can store information that will persist between calls of this function
     # This could be useful if you want to store the results heavy computations that can be cached and used across multiple calls of this function
-    # cache = problem.cache()
-    # if 'graph' not in cache:
-    #     cache['graph'] = flod_fill(problem.layout)
-    # graph = cache['graph']
+    cache = problem.cache()
+    if 'graph' not in cache:
+        cache['graph'] = flod_fill(problem.layout)
+        # for l in cache['graph']:
+        #     print(l)
+    graph = cache['graph']
     is_dead_lock = check_dead_lock(problem.layout, state, problem)
-    return min(manhattan_distance(state.player, crate) for crate in state.crates) + sum(
-        [min([manhattan_distance(crate, goal) for goal in problem.layout.goals]) for crate in
-         state.crates]) - 1 + 100 * is_dead_lock
+    res = 10000 * is_dead_lock
+    res += sum(graph[crate.y][crate.x] for crate in state.crates)
+    res += (min(manhattan_distance(state.player, crate) for crate in state.crates) - 1)
+    # res += sum([min([manhattan_distance(crate, goal) for goal in problem.layout.goals]) for crate in state.crates])
+    # res += sum([min([euclidean_distance(crate, goal) for goal in problem.layout.goals]) for crate in state.crates])
+    return res
